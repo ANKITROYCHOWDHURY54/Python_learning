169ebb2d9ca7043b2ac077944f61112194d4b224
ANKITROYCHOWDHURY54 <22p31a0504@acet.ac.in>
2024-12-29 21:18:53 +0530

    varibles and data types

---
+++ Variables_and_Data types/First_lecture_program.py
@@ -0,0 +1,37 @@
+print("Ankit Roy Chowdhury","is a good guy")
+print("Ankit Roy Chowdhury")
+print("is a good guy") 
+
+# variables & Data Types
+num = 23
+name = "Ankit Roy Chowdhury"
+price = 25.99
+real = False
+print("My name is",name,"and my age is",num,"and the price is",price,"and the real is",real)
+
+print(type(num))
+print(type(name))
+print(type(price ))
+
+a = 23
+b = 25
+# Assignment operators
+sum = a + b
+minus = a - b
+product = a * b
+division = a / b
+remainder = a % b
+power = a ** b
+print("The sum of ",a,"and",b,"is",sum)
+print("The minus of ",a,"and",b,"is",minus)
+print("The product of ",a,"and",b,"is",product)
+print("The division of ",a,"and",b,"is",division)
+print("The remainder of ",a,"and",b,"is",remainder)
+print("The power of ",a,"and",b,"is",power)
+
+#Relational Operators
+
+a = 23
+b = 25
+
+print(a == b)
---
+++ Variables_and_Data types/printsum.py
@@ -0,0 +1,7 @@
+a = 23
+b = 25
+# Assignment operators
+sum = a + b
+print("The sum of ",a,"and",b,"is",sum)
+
+#Relational Operators
